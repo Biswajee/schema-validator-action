d36d7b194386ac3bafd10d63d0b521618a09ab9f
Biswajee <roy.biswajeet161@gmail.com>
2023-03-26 11:38:32 +0530

    await the data before stringifying

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,7 @@ const getInputs = () => ({
 
 // Helper function to download a file
 const downloadFile = (downloadUrl: string): Promise<any> => {
-  return axios.get(downloadUrl).then((response) => JSON.stringify(response.data));
+  return axios.get(downloadUrl).then(async (response) => JSON.stringify(await response.data));
 };
 
 // Decide what to do based on the input parameters
